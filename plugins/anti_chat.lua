b6368f6357bdcdb348a0aa8310724386aca09744
sorblack-x <sorblack123@gmail.com>
2016-02-25 15:08:41 +0330

    Update anti_chat.lua

--- plugins/anti_chat.lua
+++ plugins/anti_chat.lua
@@ -34,7 +34,7 @@ return {
 }
 
 end
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
+--Copyright; @sorblack
+--Persian Translate; @sorblack
+--ch : @RSM_team
 --کپی بدون ذکر منبع حرام است
