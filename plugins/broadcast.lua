5948dab5e5b357e56ac700449e546cacdcb9f837
sorblack-x <sorblack123@gmail.com>
2016-02-25 15:18:35 +0330

    Update broadcast.lua

--- plugins/broadcast.lua
+++ plugins/broadcast.lua
@@ -23,7 +23,7 @@ return {
   },
   run = run
 }
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
+--Copyright; @sorblack
+--Persian Translate; @sorblack
+--ch : @RSM_team
 --کپی بدون ذکر منبع حرام است
