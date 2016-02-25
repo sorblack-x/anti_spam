8e544abe8ea5f0da7b1011f5d4399039780891c4
sorblack-x <sorblack123@gmail.com>
2016-02-25 15:34:02 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,17 +1,26 @@
 do
 
 function run(msg, matches)
-  return 'NOD32-BOT v8'.. VERSION .. [[ 
+  return 'RSMBOT v7.'.. VERSION .. [[ 
  
-  Developer : @Behroozyaghi
+  Developer and sudo : @sorblack , @sorblack_1 , @HaNgHeR_YaGhI
   
-  Channel : @Nod32team
+  special thanks and admin for help:
+  @Arsalan_sahel
+  @J2020J
+  @king_kineh
+  @Blackwolf_admin 
+  @sorblack_2 
+  @mohammad20162015 
+  @Amirho3inf
   
-  Open git : https://github.com/BH-YAGHI/NOD32-BOT.git
+  Channel : @RSM_team 
   
-  NOD32 VERSION 8
+  photo bot  : https://www.instagram.com/p/BB25RCus5ab/
   
-  All rights reserved.
+  RSMBOT VERSION 7.2
+  
+  help you : you join channel @RSM_team .
   __________________]]
 end
 
@@ -19,13 +28,13 @@ return {
   description = "Shows bot version", 
   usage = "!version: Shows bot version",
   patterns = {
-    "^!version$"
+    "^!RSMBOT$"
   }, 
   run = run 
 }
 
 end
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
+--Copyright; @sorblack
+--Persian Translate; @sorblack
+--ch : @RSM_team
 --کپی بدون ذکر منبع حرام است
