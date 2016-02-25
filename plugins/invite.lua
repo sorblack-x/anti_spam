6271520244436e2968f42855bad0ff0fa0ecb710
sorblack-x <sorblack123@gmail.com>
2016-02-25 15:22:44 +0330

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -78,7 +78,7 @@ return {
 
 end
 
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
+--Copyright; @sorblack
+--Persian Translate; @sorblack
+--ch : @RSM_team
 --کپی بدون ذکر منبع حرام است
