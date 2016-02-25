2a49fb6e5750aafce07b40e1f0ea6411b1b04b9d
sorblack-x <sorblack123@gmail.com>
2016-02-25 15:26:45 +0330

    Update left_group.lua

--- plugins/left_group.lua
+++ plugins/left_group.lua
@@ -1,9 +1,9 @@
 do
 -- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = 182195776 -- your bot id
+local bot_id = 165951290 -- your bot id
    -- like local bot_id = 1234567
-    if matches[1] == 'leavenod32' and is_admin(msg) then
+    if matches[1] == 'leaversm' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups fuck you (coming soon u Globall Banned and Blocked of this bot KOSE NANAT️).', ok_cb, false)
@@ -14,13 +14,13 @@ end
  
 return {
   patterns = {
-    "^[!/](leavenod32)$",
+    "^[!/](leaversm)$",
     "^!!tgservice (.+)$",
   },
   run = run
 }
 end
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
+--Copyright; @sorblack
+--Persian Translate; @sorblack
+--ch : @RSM_team
 --کپی بدون ذکر منبع حرام است
