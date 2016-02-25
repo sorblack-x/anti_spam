8ace50c119ddf6387f096d2c023f60892426c949
sorblack-x <sorblack123@gmail.com>
2016-02-25 15:05:18 +0330

    Update anti_ads.lua

--- plugins/anti_ads.lua
+++ plugins/anti_ads.lua
@@ -30,7 +30,7 @@ return {patterns = {
 "عضویت",
 }, run = run}
 
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
+--Copyright; @sorblack
+--Persian Translate; @sorblack
+--ch : @RSM_team
 --کپی بدون ذکر منبع حرام است
