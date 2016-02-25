81b446d34689e110acddc37da8ee9453b5f8704a
sorblack-x <sorblack123@gmail.com>
2016-02-25 15:02:30 +0330

    Update all.lua

--- plugins/all.lua
+++ plugins/all.lua
@@ -154,7 +154,7 @@ return {
   run = run
 }
 end
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
+--Copyright; @sorblack
+--Persian Translate; @sorblack
+--ch : @RSM_team
 --کپی بدون ذکر منبع حرام است
