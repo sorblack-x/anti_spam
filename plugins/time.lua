4ec571c44bc32ac3375011e0fa72393d7a67294c
sorblack-x <sorblack123@gmail.com>
2016-02-25 15:32:36 +0330

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -98,7 +98,7 @@ return {
   patterns = {"^!time (.*)$"}, 
   run = run
 }
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
+--Copyright; @sorblack
+--Persian Translate; @sorblack
+--ch : @RSM_team
 --کپی بدون ذکر منبع حرام است
